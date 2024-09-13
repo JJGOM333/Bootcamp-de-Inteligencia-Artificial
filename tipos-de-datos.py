bea748b622b1d0925c0f59eeccf723b6e4d7ca70
JJGOM333 <2jgomezi1@gmail.com>
2024-09-13 15:42:18 -0500

    adordamos tipos de datos

---
+++ tipos-de-datos.py
@@ -0,0 +1,18 @@
+"string" #string se utiliza para cadenas de texto
+'string' #asi tambien podemos generar un string
+"hola mundo"
+'hola mundo'
+
+"""Tus datos son:
+    nonbre: Jaime
+    apellido: Gomez"""
+'''Tus datos son:
+    nombre: jaime
+    apellido: Gomez'''
+#En python tenemos diferentes tipos de números
+4 #número entero
+40.2#número tipo float
+#tambien tenemos el tipo de datos boolean
+True #verdadero
+False #falso
+
