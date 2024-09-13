40122edd595b67b9f1812cb30af39073d63fe59a
JJGOM333 <2jgomezi1@gmail.com>
2024-09-13 17:21:53 -0500

    Abordamos hasta el slide 203 del material de apoyo

---
+++ variable.py
@@ -0,0 +1,12 @@
+a = 5
+b = 8
+c = a + b
+print(c)
+nombre = "Jaime"
+print(nombre)
+nombre = 40 + 20
+print(nombre)
+nombre = "Jaime"
+nombre = "JJGOMEZ"
+print(nombre)
+#podemos concluir que las variables son modificables
