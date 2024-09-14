a56aa206f0e7ad0f3fa38b39f3a8862aae15e788
JJGOM333 <2jgomezi1@gmail.com>
2024-09-13 19:35:25 -0500

    completamos el archivo variable

--- tipos-de-datos.py
+++ tipos-de-datos.py
@@ -1,5 +1,6 @@
 "string" #string se utiliza para cadenas de texto
 'string' #asi tambien podemos generar un string
+'''string'''#por ejemplo asi también
 "hola mundo"
 'hola mundo'
 
@@ -9,10 +10,11 @@
 '''Tus datos son:
     nombre: jaime
     apellido: Gomez'''
+    
 #En python tenemos diferentes tipos de números
 4 #número entero
 40.2#número tipo float
-#tambien tenemos el tipo de datos boolean
+#tambien tenemos el tipo de datos (bool) True o False
 True #verdadero
 False #falso
 
